68f041a94ffa3f0348c465c81df9083932fb4d63
Suraj Veerappa Bolanavar <SBolanavar@synamedia.com>
2021-02-21 21:32:05 +0530

    Fixing the single module loading error because we made the faker module shared

--- shopping-cart/cart/src/index.js
+++ shopping-cart/cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-const cartItems = `<div> you have ${faker.random.number()} items in your cart </div>`;
-
-document.querySelector('#dev-cart').innerHTML = cartItems;
+import('./bootstrap')
